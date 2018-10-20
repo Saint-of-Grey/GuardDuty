f3bdaa56512d739209bf3ba3dadfaaaae74e4a03
Alice Cecil <alycecil@gmail.com>
2018-10-20 06:41:21 -0400

    Initial Version

---
+++ Source/GuardDuty.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace GuardDuty
+{
+    public class GuardDuty : Mod
+    {
+        public static MyModSettings latest;
+        private MyModSettings _settings;
+
+        public GuardDuty(ModContentPack content) : base(content)
+        {
+            latest = this._settings = GetSettings<MyModSettings>();
+        }
+
+        public override string SettingsCategory() => "Guard Duty";
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            var topHalf = inRect.TopHalf();
+            var fourth = topHalf.TopHalf();
+            var mySlice = fourth.TopHalf();
+            
+            
+            Widgets.CheckboxLabeled(mySlice.RightHalf().ContractedBy(4f), "Auto Capture", ref _settings.autoCapture);
+            //Widgets.CheckboxLabeled(mySlice.LeftHalf().ContractedBy(4f), "", ref _settings.autoCapture);
+        }
+    }
+    
+    public class MyModSettings : ModSettings
+    {
+        public bool autoCapture = true;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref this.autoCapture, "autoCapture", true);
+            
+        }
+
+       
+    }
+}
---
+++ Source/ThinkNode_ConditionalWorkTypeDef.cs
@@ -0,0 +1,32 @@
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace GuardDuty
+{
+    public class ThinkNode_ConditionalWorkTypeDef : ThinkNode_Conditional
+    {
+        private WorkTypeDef _def;
+        private string workTypeDef;
+        private bool makeAgro;
+
+        protected override bool Satisfied(Pawn me)
+        {
+            if (_def == null)
+                _def = DefDatabase<WorkTypeDef>.GetNamedSilentFail(workTypeDef);
+
+            if (_def == null) return false;
+
+            if (!me.workSettings.WorkIsActive(_def))
+                return false;
+
+            if (makeAgro)
+            {
+                if (me.playerSettings == null) me.playerSettings = new Pawn_PlayerSettings(me);
+                me.playerSettings.hostilityResponse = HostilityResponseMode.Attack;
+            }
+
+            return true;
+        }
+    }
+}
