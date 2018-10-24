a6366022933e323012576e07ef9827e551f98090
Alice Cecil <alycecil@gmail.com>
2018-10-24 17:18:35 -0400

    Lower prioirtiy

--- Source/JobGiver_GoToBell.cs
+++ Source/JobGiver_GoToBell.cs
@@ -1,16 +1,35 @@
+using RimWorld;
 using Verse;
 using Verse.AI;
 
 namespace GuardDuty
 {
-    public class JobGiver_GoToBell : JobGiver_Wander
+    public class JobGiver_GoToBell : JobGiver_WanderColony
     {
         protected override IntVec3 GetWanderRoot(Pawn pawn)
         {
+            if (pawn == null)
+            {
+                return Find.CurrentMap.Center;
+            }
+
+            ThingDef singleDef = WhatDef();
+
+            var thingRequest = ThingRequest.ForDef(singleDef);
+            return ClosestPosition(pawn, thingRequest)?.Position ?? base.GetWanderRoot(pawn);
+        }
+
+        private static Thing ClosestPosition(Pawn pawn, ThingRequest thingRequest)
+        {
             return GenClosest.ClosestThingReachable(pawn.Position, pawn.Map,
-                ThingRequest.ForDef(WhatDef()), PathEndMode.OnCell,
-                TraverseParms.For(pawn, Danger.Some),
-                7f).Position;
+                thingRequest, PathEndMode.Touch,
+                Danger(pawn),
+                200f);
+        }
+
+        private static TraverseParms Danger(Pawn pawn)
+        {
+            return TraverseParms.For(pawn, Verse.Danger.Some);
         }
 
         protected virtual ThingDef WhatDef()
