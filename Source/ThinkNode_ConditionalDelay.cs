319fce5331669ee4913afe50d3978fd220c91e92
Saint of Grey <42332565+Saint-of-Grey@users.noreply.github.com>
2018-11-09 18:31:54 -0800

    Revert "Revert "Add delay""
    This reverts commit 45109921df4bcfd5fb8420bc48f8d2a3b4aa2114.

---
+++ Source/ThinkNode_ConditionalDelay.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace GuardDuty
+{
+    public class ThinkNode_ConditionalDelay : ThinkNode_Conditional
+    {
+        public int min = 1000, max= 20000;
+
+        private Dictionary<string, int> delays = new Dictionary<string, int>();
+        protected override bool Satisfied(Pawn pawn)
+        {
+            var key = GetKey(pawn);
+            if (delays.TryGetValue(key, out var tick))
+            {
+                var tik = Find.TickManager.TicksGame;
+
+                if (tik > tick)
+                {
+                    AddDelay(key);
+                    return true;
+                }
+            }
+            else
+            {
+                AddDelay(key);
+            }
+            return false;
+        }
+
+        public virtual string GetKey(Pawn pawn)
+        {
+            return pawn.GetUniqueLoadID();
+        }
+
+        public virtual void AddDelay(string key)
+        {
+            delays[key] = Find.TickManager.TicksGame + Rand.Range(min, max);
+        }
+    }
+}
